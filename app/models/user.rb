41ca7e006207cefc44c346eade1695f461293612
1satoshik7 <satoshi.kaneko1207@gmail.com>
2020-10-26 11:03:10 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :date, presence: true
+  validates :nickname, presence: true
+  validates :sur_name, presence: true
+  validates :name, presence: true
+  validates :sur_name_ruby, presence: true
+  validates :name_ruby, presence: true
 end
